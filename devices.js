f3a746b2994a2c9ed52535aba2c0eee4a17ecac0
Juergen <einsuperburschi@gmx.de>
2020-11-02 17:41:23 +0100

    add converter schedule, week, window_detect

--- devices.js
+++ devices.js
@@ -1657,6 +1657,7 @@ const devices = [
         ],
         meta: {tuyaThermostatSystemMode: common.TuyaThermostatSystemModes, tuyaThermostatPreset: common.TuyaThermostatPresets},
         ota: ota.zigbeeOTA,
+        onEvent: tuya.setTime,
         fromZigbee: [fz.tuya_thermostat, fz.tuya_thermostat_on_set_data, fz.ignore_basic_report],
         toZigbee: [
             tz.tuya_thermostat_child_lock, tz.tuya_thermostat_valve_detection,
@@ -1664,7 +1665,7 @@ const devices = [
             tz.tuya_thermostat_calibration, tz.tuya_thermostat_min_temp, tz.tuya_thermostat_max_temp,
             tz.tuya_thermostat_boost_time, tz.tuya_thermostat_comfort_temp, tz.tuya_thermostat_eco_temp,
             tz.tuya_thermostat_force, tz.tuya_thermostat_preset, tz.tuya_thermostat_away_mode,
-            tz.tuya_thermostat_window_detect, tz.tuya_thermostat_schedule, tz.tuya_thermostat_week,
+            tz.tuya_thermostat_window_detect, tz.tuya_thermostat_schedule, tz.tuya_thermostat_week, 
         ],
     },
     {
